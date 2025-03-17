1d5a03fd88e0f0fa1977a7088e5d2e86fa6d15c5
dilshod abdirazzoqov <abdirazzoqovd@sgcc.uz>
2025-03-17 11:57:13 +0500

    last updates here

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,7 @@ import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 
 const Dashboard = () => {
-  const [user, setUser] = useState(null);
+  const [user, setUser ] = useState(null);
   const [kpiData, setKpiData] = useState([]);
   const navigate = useNavigate();
   const pdfRef = useRef();
@@ -14,14 +14,14 @@ const Dashboard = () => {
   const API_URL = "http://localhost:8055/items/kpi_cards";
 
   useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem("user"));
-    if (!storedUser) {
+    const storedUser  = JSON.parse(localStorage.getItem("user"));
+    if (!storedUser ) {
       navigate("/login");
       return;
     }
-    setUser(storedUser);
+    setUser (storedUser );
 
-    fetch(`${API_URL}?filter[xodim_id][_eq]=${storedUser.id}`, {
+    fetch(`${API_URL}?filter[xodim_id][_eq]=${storedUser .id}`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -55,7 +55,7 @@ const Dashboard = () => {
       {user ? (
         <>
           <div className="flex justify-between items-center">
-            <h1 className="text-2xl font-bold">{user.xodim[0]}</h1>
+            <h1 className="text-2xl font-bold">{user.xodim[0] || "Bo'sh"}</h1>
             <button
               onClick={handleLogout}
               className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition-all"
@@ -77,35 +77,35 @@ const Dashboard = () => {
               <tbody className="text-[.6rem]">
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">Tarkibiy bo‘linman nomi</td>
-                  <td className="border border-black px-2 py-1\">{user.tarkibiy_bolinma_nomi}</td>
+                  <td className="border border-black px-2 py-1">{user.tarkibiy_bolinma_nomi || "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">Xodimning lavozimi</td>
-                  <td className="border border-black px-2 py-1">{user.xodim_lavozimi[0]}</td>
+                  <td className="border border-black px-2 py-1">{user.xodim_lavozimi[0] || "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">Xodim F.I.SH.</td>
-                  <td className="border border-black px-2 py-1">{user.xodim[0]}</td>
+                  <td className="border border-black px-2 py-1">{user.xodim[0] || "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">Funksional rahbar F.I.SH.</td>
-                  <td className="border border-black px-2 py-1">{user.funksional_raxbar[0]}</td>
+                  <td className="border border-black px-2 py-1">{user.funksional_raxbar[0] || "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">Ma’muriy rahbar F.I.SH.</td>
-                  <td className="border border-black px-2 py-1">{user.mamuriy_raxbar[0]}</td>
+                  <td className="border border-black px-2 py-1">{user.mamuriy_raxbar[0] || "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">Baxolash davri</td>
-                  <td className="border border-black px-2 py-1">{user.baholash_davri[0]}</td>
+                  <td className="border border-black px-2 py-1">{user.baholash_davri[0] || "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">1-Bosqich.Xodim imzosi</td>
-                  <td className="border border-black px-2 py-1">{user.birinchi_bosqich_imzo.replace("T", " ")}</td>
+                  <td className="border border-black px-2 py-1">{user.birinchi_bosqich_imzo ? user.birinchi_bosqich_imzo.replace("T", " ") : "Bo'sh"}</td>
                 </tr>
                 <tr>
                   <td className="border border-black px-2 py-1 font-bold">2-Bosqich.Xodim imzosi</td>
-                  <td className="border border-black px-2 py-1">{user.ikkinchi_bosqich_imzo.replace("T", " ")}</td>
+                  <td className="border border-black px-2 py-1">{user.ikkinchi_bosqich_imzo ? user.ikkinchi_bosqich_imzo.replace("T", " ") : "Bo'sh"}</td>
                 </tr>
               </tbody>
             </table>
@@ -139,20 +139,20 @@ const Dashboard = () => {
                     {kpiData.map((kpi, index) => (
                       <tr key={kpi.id} className="h-12 text-[.6rem]">
                         <td className="border border-black px-2 py-1 text-center">{index + 1}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.maqsad}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.kpi_nomi}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.xisoblash_metodikasi}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.malumotlar_manbai}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.olchov_birlik}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.reja}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.maqsadning_haqiqiy_miqdori}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.salmoq_kutilayotgan}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.salmoq_real}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.izoh}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.baholovchi_sex[0]}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.baholovchi_xodim[0]}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.birinchi_bosqich_sana.replace("T", " ")}</td>
-                        <td className="border border-black px-2 py-1 text-center">{kpi.ikkinchi_bosqich_sana.replace("T", " ")}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.maqsad || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.kpi_nomi || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.xisoblash_metodikasi || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.malumotlar_manbai || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.olchov_birlik || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.reja || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.maqsadning_haqiqiy_miqdori || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.salmoq_kutilayotgan || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.salmoq_real || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.izoh || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.baholovchi_sex[0] || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.baholovchi_xodim[0] || "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.birinchi_bosqich_sana ? kpi.birinchi_bosqich_sana.replace("T", " ") : "Bo'sh"}</td>
+                        <td className="border border-black px-2 py-1 text-center">{kpi.ikkinchi_bosqich_sana ? kpi.ikkinchi_bosqich_sana.replace("T", " ") : "Bo'sh"}</td>
                       </tr>
                     ))}
                   </tbody>
